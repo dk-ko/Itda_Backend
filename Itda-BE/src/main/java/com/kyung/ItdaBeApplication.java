f985b2fdabee2d4ce1e12f333d47364b4f6d3608
Dakyung Ko <kdkyung57@gmail.com>
2018-09-19 22:54:10 +0900

    chore: Add gitignore

---
+++ Itda-BE/src/main/java/com/kyung/ItdaBeApplication.java
@@ -0,0 +1,12 @@
+package com.kyung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItdaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItdaBeApplication.class, args);
+	}
+}
